6ea67c5f2eef18c618b21cd3df9e7f9c04f9412f
Michael McDonald <mkmcdona@gmail.com>
2018-12-09 13:54:56 -0800

    Add routing for settings and rename home to map

--- src/router.js
+++ src/router.js
@@ -1,24 +1,41 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+
+import store from "./store";
+import Map from "./views/Map.vue";
 
 Vue.use(Router);
+
+function requireAccessToken(to, from, next) {
+  if (!store.state.map.accessToken) {
+    next({
+      path: "/settings"
+    });
+  } else {
+    next();
+  }
+}
 
 export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      redirect: "/map"
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/map",
+      name: "map",
+      component: Map,
+      beforeEnter: requireAccessToken
+    },
+    {
+      path: "/settings",
+      name: "settings",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "about" */ "./views/Settings.vue")
     }
   ]
 });
