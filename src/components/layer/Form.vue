865f9731a534ebe9bb81c2ac98dab63170dafd55
Michael McDonald <mkmcdona@gmail.com>
2018-12-17 15:01:05 -0800

    Add FileUpload component to Form component

--- src/components/layer/Form.vue
+++ src/components/layer/Form.vue
@@ -10,33 +10,29 @@
       @input="update('name', $event)"
     ></Textfield>
     <div>
-      <ul class="list-reset flex justify-between">
-        <li class="mr-3">
-          <span
-            class="inline-block border border-blue rounded py-2 px-4 bg-blue hover:bg-blue-dark text-white"
-          >Data</span>
-        </li>
-        <li class="mr-3">
-          <span
-            class="inline-block border border-white rounded hover:border-grey-lighter text-blue hover:bg-grey-lighter py-2 px-4"
-          >Filters</span>
-        </li>
-        <li class="mr-3">
-          <span class="inline-block py-2 px-4 text-grey-light cursor-not-allowed" href="#">Analytics</span>
-        </li>
-      </ul>
+      <FileUpload
+        title="Name"
+        class="mb-3"
+        id="name"
+        placeholder="Choose a file to load data from..."
+        required="true"
+        :value="layer.data"
+        @input="addData"
+      ></FileUpload>
     </div>
   </div>
 </template>
 
 <script>
 import Textfield from "@/components/form/Textfield.vue";
+import FileUpload from "@/components/form/FileUpload.vue";
 import { mapActions } from "vuex";
 
 export default {
   name: "Form",
   components: {
-    Textfield
+    Textfield,
+    FileUpload
   },
   props: ["index", "layer"],
   methods: {
@@ -45,6 +41,10 @@ export default {
     }),
     update(prop, value) {
       this.layer[prop] = value;
+      this.save({ index: this.index, layer: this.layer });
+    },
+    addData(contents) {
+      this.layer.data = JSON.parse(contents);
       this.save({ index: this.index, layer: this.layer });
     }
   }
