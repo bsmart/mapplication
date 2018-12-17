3040d3bb0421509abd958188b494938a286250a8
Michael McDonald <mkmcdona@gmail.com>
2018-12-17 12:45:23 -0800

    Add some more margins to settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,11 +1,12 @@
 <template>
   <form id="settings" class="mt-8 container mx-auto px-4">
     <h1 class="mb-8">Settings</h1>
-    <h2 class="mb-6">Mapbox</h2>
+    <h2 class="mb-6 pb-1 border-b">Mapbox</h2>
     <Textfield
       title="Access token"
       class="mb-3"
       id="accessToken"
+      required="true"
       placeholder="Enter your Mapbox access token..."
       v-model="tokenValue"
     ></Textfield>
@@ -16,7 +17,7 @@
       placeholder="Enter a Mapbox style url..."
       v-model="styleValue"
     ></Textfield>
-    <h2 class="mb-6">Map center</h2>
+    <h2 class="mb-6 pb-1 border-b">Map center</h2>
     <section class="flex items-center justify-between -mx-3">
       <Textfield
         title="Longitude"
